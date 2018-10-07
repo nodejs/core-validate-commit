3032b71239273f2192abb7e6c6d24406686bf231
Rich Trott <rtrott@gmail.com>
2018-10-07 16:52:51 -0700

    tests and bugfix for line-after-title (#29)
    * add basic tests for line-after-title rule
    
    * fix line-after-title for commits without a body

---
+++ test/rules/line-after-title.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const test = require('tap').test
+const Rule = require('../../lib/rules/line-after-title')
+const Commit = require('gitlint-parser-node')
+const Validator = require('../../')
+
+test('rule: line-after-title', (t) => {
+  t.test('no blank line', (tt) => {
+    tt.plan(7)
+    const v = new Validator()
+    const context = new Commit({
+      sha: 'e7c077c610afa371430180fbd447bfef60ebc5ea'
+    , author: {
+        name: 'Evan Lucas'
+      , email: 'evanlucas@me.com'
+      , date: '2016-04-12T19:42:23Z'
+      }
+    , message: 'test: fix something\nfhqwhgads'
+    }, v)
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'line-after-title', 'id')
+      tt.equal(opts.message, 'blank line expected after title', 'message')
+      tt.equal(opts.string, 'fhqwhgads', 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 0, 'column')
+      tt.equal(opts.level, 'fail', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('blank line', (tt) => {
+    tt.plan(4)
+    const v = new Validator()
+    const context = new Commit({
+      sha: 'e7c077c610afa371430180fbd447bfef60ebc5ea'
+    , author: {
+        name: 'Evan Lucas'
+      , email: 'evanlucas@me.com'
+      , date: '2016-04-12T19:42:23Z'
+      }
+    , message: 'test: fix something\n\nfhqwhgads'
+    }, v)
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'line-after-title', 'id')
+      tt.equal(opts.message, 'blank line after title', 'message')
+      tt.equal(opts.level, 'pass', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('just one line', (tt) => {
+    tt.plan(4)
+    const v = new Validator()
+    const context = new Commit({
+      sha: 'e7c077c610afa371430180fbd447bfef60ebc5ea'
+    , author: {
+        name: 'Evan Lucas'
+      , email: 'evanlucas@me.com'
+      , date: '2016-04-12T19:42:23Z'
+      }
+    , message: 'test: fix something'
+    }, v)
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'line-after-title', 'id')
+      tt.equal(opts.message, 'blank line after title', 'message')
+      tt.equal(opts.level, 'pass', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.end()
+})
