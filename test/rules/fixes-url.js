nodejs/core-validate-commit

commit 2523fb9960ba07b1c8dd1d9c54c33ceef49bef0f

rules: stricter `Fixes:` URL check
Only allow GitHub issue URLs, or comments or discussions from pull
request URLs.

lib/rules/fixes-url.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const id = 'fixes-url'
+const github = new RegExp('^https://github\.com/\\w+\/\\w+/' +
+  '(issues|pull)/\\d+(#issuecomment-\\d+|#discussion_r\\d+)?$'
+)
 
 module.exports = {
   id: id
@@ -21,28 +24,53 @@ module.exports = {
       })
       return
     }
+    // Allow GitHub issues with optional comment.
+    // GitHub pull requests must reference a comment or discussion.
     for (const url of parsed.fixes) {
+      const match = github.exec(url)
       if (url[0] === '#') {
         // See nodejs/node#2aa376914b621018c5784104b82c13e78ee51307
         // for an example
         const { line, column } = findLineAndColumn(context.body, url)
         context.report({
           id: id
-        , message: 'Fixes must be a url, not an issue number.'
+        , message: 'Fixes must be a URL, not an issue number.'
         , string: url
         , line: line
         , column: column
         , level: 'fail'
         })
+      } else if (match) {
+        if (match[1] === 'pull' && match[2] === undefined) {
+          const { line, column } = findLineAndColumn(context.body, url)
+          context.report({
+            id: id
+          , message: 'Pull request URL must reference a comment or discussion.'
+          , string: url
+          , line: line
+          , column: column
+          , level: 'fail'
+          })
+        } else {
+          const { line, column } = findLineAndColumn(context.body, url)
+          context.report({
+            id: id
+          , message: 'Valid fixes URL.'
+          , string: url
+          , line: line
+          , column: column
+          , level: 'pass'
+          })
+        }
       } else {
         const { line, column } = findLineAndColumn(context.body, url)
         context.report({
           id: id
-        , message: 'Valid fixes url'
+        , message: 'Fixes must be a GitHub URL.'
         , string: url
         , line: line
         , column: column
-        , level: 'pass'
+        , level: 'fail'
         })
       }
     }

test/rules/fixes-url.js
@@ -4,29 +4,162 @@ const test = require('tap').test
 const Rule = require('../../lib/rules/fixes-url')
 const Commit = require('gitlint-parser-node')
 const Validator = require('../../')
-const INVALID_FIXES_URL = 'Fixes must be a url, not an issue number.'
+
+const INVALID_PRURL = 'Pull request URL must reference a comment or discussion.'
+const NOT_AN_ISSUE_NUMBER = 'Fixes must be a URL, not an issue number.'
+const NOT_A_GITHUB_URL = 'Fixes must be a GitHub URL.'
+const VALID_FIXES_URL = 'Valid fixes URL.'
+
+const makeCommit = (msg) => {
+  return new Commit({
+    sha: 'e7c077c610afa371430180fbd447bfef60ebc5ea'
+  , author: {
+      name: 'Evan Lucas'
+    , email: 'evanlucas@me.com'
+    , date: '2016-04-12T19:42:23Z'
+    }
+  , message: msg
+  }, new Validator())
+}
 
 test('rule: fixes-url', (t) => {
-  t.test('invalid', (tt) => {
+  t.test('issue number', (tt) => {
     tt.plan(7)
-    const v = new Validator()
-    const context = new Commit({
-      sha: 'e7c077c610afa371430180fbd447bfef60ebc5ea'
-    , author: {
-        name: 'Evan Lucas'
-      , email: 'evanlucas@me.com'
-      , date: '2016-04-12T19:42:23Z'
-      }
-    , message: `test: fix something
+    const context = makeCommit(`test: fix something
 
 Fixes: #1234`
-    }, v)
+    )
 
     context.report = (opts) => {
       tt.pass('called report')
       tt.equal(opts.id, 'fixes-url', 'id')
-      tt.equal(opts.message, INVALID_FIXES_URL, 'message')
+      tt.equal(opts.message, NOT_AN_ISSUE_NUMBER, 'message')
       tt.equal(opts.string, '#1234', 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 7, 'column')
+      tt.equal(opts.level, 'fail', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('GitHub issue URL', (tt) => {
+    tt.plan(7)
+    const url = 'https://github.com/nodejs/node/issues/1234'
+    const context = makeCommit(`test: fix something
+
+Fixes: ${url}`
+    )
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'fixes-url', 'id')
+      tt.equal(opts.message, VALID_FIXES_URL, 'message')
+      tt.equal(opts.string, url, 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 7, 'column')
+      tt.equal(opts.level, 'pass', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('GitHub issue URL with comment', (tt) => {
+    tt.plan(7)
+    const url = 'https://github.com/nodejs/node/issues/1234#issuecomment-1234'
+    const context = makeCommit(`test: fix something
+
+Fixes: ${url}`
+    )
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'fixes-url', 'id')
+      tt.equal(opts.message, VALID_FIXES_URL, 'message')
+      tt.equal(opts.string, url, 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 7, 'column')
+      tt.equal(opts.level, 'pass', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('GitHub PR URL', (tt) => {
+    tt.plan(7)
+    const url = 'https://github.com/nodejs/node/pull/1234'
+    const context = makeCommit(`test: fix something
+
+Fixes: ${url}`
+    )
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'fixes-url', 'id')
+      tt.equal(opts.message, INVALID_PRURL, 'message')
+      tt.equal(opts.string, url, 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 7, 'column')
+      tt.equal(opts.level, 'fail', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('GitHub PR URL with comment', (tt) => {
+    tt.plan(7)
+    const url = 'https://github.com/nodejs/node/pull/1234#issuecomment-1234'
+    const context = makeCommit(`test: fix something
+
+Fixes: ${url}`
+    )
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'fixes-url', 'id')
+      tt.equal(opts.message, VALID_FIXES_URL, 'message')
+      tt.equal(opts.string, url, 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 7, 'column')
+      tt.equal(opts.level, 'pass', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('GitHub PR URL with discussion comment', (tt) => {
+    tt.plan(7)
+    const url = 'https://github.com/nodejs/node/pull/1234#discussion_r1234'
+    const context = makeCommit(`test: fix something
+
+Fixes: ${url}`
+    )
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'fixes-url', 'id')
+      tt.equal(opts.message, VALID_FIXES_URL, 'message')
+      tt.equal(opts.string, url, 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 7, 'column')
+      tt.equal(opts.level, 'pass', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
+  t.test('non-GitHub URL', (tt) => {
+    tt.plan(7)
+    const context = makeCommit(`test: fix something
+
+Fixes: https://nodejs.org`
+    )
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'fixes-url', 'id')
+      tt.equal(opts.message, NOT_A_GITHUB_URL, 'message')
+      tt.equal(opts.string, 'https://nodejs.org', 'string')
       tt.equal(opts.line, 1, 'line')
       tt.equal(opts.column, 7, 'column')
       tt.equal(opts.level, 'fail', 'level')

commit a9bd5d7d2e7aa438d4a06d800e2fdf15a06e388a

rules: allow dashes in 'Fixes:' URL check
Before this commit, links to https://github.com/nodejs/node-gyp/issues
were rejected because of the dash in 'node-gyp'.

Notably, this fix means this commit log now passes validation. :-)

Bug introduced in commit 2523fb9 ("rules: stricter `Fixes:` URL check")
from March 30.

Fixes: https://github.com/nodejs/core-validate-commit/issues/60

lib/rules/fixes-url.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const id = 'fixes-url'
-const github = new RegExp('^https://github\.com/\\w+\/\\w+/' +
+const github = new RegExp('^https://github\.com/[\\w\\-]+\/[\\w\\-]+/' +
   '(issues|pull)/\\d+(#issuecomment-\\d+|#discussion_r\\d+)?$'
 )
 

test/rules/fixes-url.js
@@ -64,6 +64,27 @@ Fixes: ${url}`
     Rule.validate(context)
   })
 
+  t.test('GitHub issue URL with dash', (tt) => {
+    tt.plan(7)
+    const url = 'https://github.com/foo-bar/bar-baz/issues/1234'
+    const context = makeCommit(`test: fix something
+
+Fixes: ${url}`
+    )
+
+    context.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'fixes-url', 'id')
+      tt.equal(opts.message, VALID_FIXES_URL, 'message')
+      tt.equal(opts.string, url, 'string')
+      tt.equal(opts.line, 1, 'line')
+      tt.equal(opts.column, 7, 'column')
+      tt.equal(opts.level, 'pass', 'level')
+    }
+
+    Rule.validate(context)
+  })
+
   t.test('GitHub issue URL with comment', (tt) => {
     tt.plan(7)
     const url = 'https://github.com/nodejs/node/issues/1234#issuecomment-1234'