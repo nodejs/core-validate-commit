dd5ffe0b3a2a98a41b2bfab80fe7a31826771248
Lucas Holmquist <lholmqui@redhat.com>
2019-08-05 06:54:35 -0400

    squash: use tap.throws instead of try/catch

--- test/rule-test.js
+++ test/rule-test.js
@@ -5,27 +5,19 @@ const BaseRule = require('../lib/rule')
 
 test('Base Rule Test', (t) => {
   t.test('No id param', (tt) => {
-    try {
+    tt.throws(() => {
       new BaseRule()
-      tt.fail('This should fail')
-    } catch (err) {
-      tt.equal(err.message,
-               'Rule must have an id',
-               'Should have error message about id')
-      tt.end()
-    }
+    }, 'Rule must have an id')
+
+    tt.end()
   })
 
   t.test('No validate function', (tt) => {
-    try {
+    tt.throws(() => {
       new BaseRule({id: 'test-rule'})
-      tt.fail('This should fail')
-    } catch (err) {
-      tt.equal(err.message,
-               'Rule must have validate function',
-               'Should have error message about validate function')
-      tt.end()
-    }
+    }, 'Rule must have validate function')
+
+    tt.end()
   })
 
   t.end()
