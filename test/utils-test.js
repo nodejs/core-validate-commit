3be18a59c2c3ed41b935be520d526507f8bc5d23
Lucas Holmquist <lholmqui@redhat.com>
2019-08-07 08:38:53 -0400

    test: add tests for header util functions
    * This adds a test for the header utility function.

--- test/utils-test.js
+++ test/utils-test.js
@@ -3,6 +3,10 @@
 const { test } = require('tap')
 const utils = require('../lib/utils')
 
+// We aren't testing the chalk library, so strip off the colors/styles it adds
+const stripAnsiRegex =
+/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g
+
 test('test utility functions', (t) => {
   t.test('test rightPad function - with padding', (tt) => {
     const padded = utils.rightPad('string', 10)
@@ -33,6 +37,38 @@ test('test utility functions', (t) => {
     tt.equal(padded.length, 6, 'should have the same length')
     tt.equal(padded, 'string', 'should have no padding on the left')
 
+    tt.end()
+  })
+
+  t.test('test headers function - skip', (tt) => {
+    const header = utils.header('abc123', 'skip')
+    tt.equal(header.replace(stripAnsiRegex, ''),
+             '✔  abc123 # SKIPPED',
+             'should be equal')
+    tt.end()
+  })
+
+  t.test('test headers function - pass', (tt) => {
+    const header = utils.header('abc123', 'pass')
+    tt.equal(header.replace(stripAnsiRegex, ''),
+             '✔  abc123',
+             'should be equal')
+    tt.end()
+  })
+
+  t.test('test headers function - pass', (tt) => {
+    const header = utils.header('abc123', 'pass')
+    tt.equal(header.replace(stripAnsiRegex, ''),
+             '✔  abc123',
+             'should be equal')
+    tt.end()
+  })
+
+  t.test('test headers function - fail', (tt) => {
+    const header = utils.header('abc123', 'fail')
+    tt.equal(header.replace(stripAnsiRegex, ''),
+             '✖  abc123',
+             'should be equal')
     tt.end()
   })
 
