f05715d5325da6dc65a3f3b5fa98b1e231bdec68
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2026-01-12 20:10:58 +0100

    fixup!: add contrapositive test-case

--- test/rules/line-length.js
+++ test/rules/line-length.js
@@ -130,32 +130,57 @@ https://${'very-'.repeat(80)}-long-url.org/
   })
 
   t.test('Co-author lines', (tt) => {
+    const sha = 'f1496de5a7d5474e39eafaafe6f79befe5883a5b'
+    const author = {
+      name: 'Jacob Smith',
+      email: '3012099+JakobJingleheimer@users.noreply.github.com',
+      date: '2025-12-22T09:40:42Z'
+    }
+
     const v = new Validator()
-    const context = new Commit({
-      sha: 'f1496de5a7d5474e39eafaafe6f79befe5883a5b',
-      author: {
-        name: 'Jacob Smith',
-        email: '3012099+JakobJingleheimer@users.noreply.github.com',
-        date: '2025-12-22T09:40:42Z'
-      },
-      message: `
-        fixup!: apply case-insensitive suggestion
-        Co-authored-by: Michaël Zasso <37011812+targos@users.noreply.github.com>
-      `
+    const overlongMessage = `fixup!: apply case-insensitive suggestion
+      Co-authored-by: Michaël Zasso <37011812+targos@users.noreply.github.com>`
+    const bad = new Commit({
+      sha,
+      author,
+      message: overlongMessage,
     }, v)
 
-    context.report = (opts) => {
+    bad.report = (opts) => {
+      tt.pass('called report')
+      tt.equal(opts.id, 'line-length', 'id')
+      tt.equal(opts.string, overlongMessage.split('\n')[1], 'string')
+      tt.equal(opts.level, 'fail', 'level')
+    }
+
+    Rule.validate(bad, {
+      options: {
+        length: 72
+      }
+    })
+
+    const good = new Commit({
+      sha,
+      author,
+      message: [
+        'fixup!: apply case-insensitive suggestion',
+        'Co-authored-by: Michaël Zasso <37011812+targos@users.noreply.github.com>'
+      ].join('\n')
+    }, v)
+
+    good.report = (opts) => {
       tt.pass('called report')
       tt.equal(opts.id, 'line-length', 'id')
       tt.equal(opts.string, '', 'string')
       tt.equal(opts.level, 'pass', 'level')
     }
 
-    Rule.validate(context, {
+    Rule.validate(good, {
       options: {
         length: 72
       }
     })
+
     tt.end()
   })
 
