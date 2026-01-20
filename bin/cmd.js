64424bf852db60d94895c9b7ad3c3608bf422fe2
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-01-20 22:16:46 +0100

    fixup! Update bin/cmd.js

--- bin/cmd.js
+++ bin/cmd.js
@@ -142,6 +142,7 @@ if (args.length === 1 && args[0] === '-') {
 
       // Replace args with the commit data directly
       for (let i = 0; i < commits.length; i++) {
+        const commit = commits[i]
         if (!commit.id || !commit.message) {
           throw new Error('Each commit must have "id" and "message" properties')
         }
